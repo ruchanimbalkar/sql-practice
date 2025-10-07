01ed1b701c78c1da7bf11da820c09f991744865d
Rucha Nimbalkar <rucha.nimbalkar90@gmail.com>
2025-10-07 11:44:05 -0700

    Initial commit

---
+++ murder-mystery-game.sql
@@ -0,0 +1,7 @@
+-- Record your SQL detective process here!  Write down: 
+--   1. The SQL queries you ran
+--   2. Any notes or insights as SQL comments
+--   3. Your final conclusion: who did it?
+
+-- Using these SQL clauses will help you solve the mystery: 
+--    SELECT, FROM, WHERE, AND, OR, ORDER BY, LIMIT, LIKE, DISTINCT, BETWEEN, AS
