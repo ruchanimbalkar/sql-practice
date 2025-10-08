81318eeba2cfe9926206d4b06fdd1ec5cff3ae5a
Rucha Nimbalkar <rucha.nimbalkar90@gmail.com>
2025-10-07 21:02:43 -0700

    Villain found :)
    Found villian who hired the murderer

--- murder-mystery-game.sql
+++ murder-mystery-game.sql
@@ -1,29 +1,20 @@
+-- Record your SQL detective process here!  Write down: 
+--   1. The SQL queries you ran
+--   2. Any notes or insights as SQL comments
+--   3. Your final conclusion: who did it?
+
+-- Using these SQL clauses will help you solve the mystery: 
+--    SELECT, FROM, WHERE, AND, OR, ORDER BY, LIMIT, LIKE, DISTINCT, BETWEEN, AS
+
+--Link : https://www.db-fiddle.com/f/gBhY4smJswSVNW7zNnctju/52
+--Assignment Reference : https://mystery.knightlab.com/
 --Background : Experienced SQL sleuths start here
   --A crime has taken place and the detective needs your help. 
   --The detective gave you the crime scene report, but you somehow lost it. 
   --You vaguely remember that the crime was a ​murder​ that occurred sometime on ​Jan.15, 2018​ and that it took place in ​SQL City​. 
   --Start by retrieving the corresponding crime scene report from the police department’s database.
 -- Record your SQL detective process here!  Write down: 
-
--- Run this query to see sqlite_master.
-SELECT * FROM sqlite_master;
-
--- Run this query to find the names of the tables in this database(sqlite_master).
-SELECT name FROM sqlite_master
- WHERE TYPE = 'table';
-
-
--- Run this query to find the structure of the `crime_scene_report` table
-SELECT sql FROM sqlite_master
- where name = 'crime_scene_report';
- 
--- Run this query to find the structure of the 'facebook_event_checkin' table
-SELECT sql FROM sqlite_master
- where name = 'facebook_event_checkin';
-
-SELECT * FROM sqlite_master
-ORDER BY rootpage;
-
+-----------------------------------------------------------------------------------------------------
 --SELECT * FROM crime_scene_report;
 --My first step is to find details of the crime and for that I need to find the crime report.
 --A murder occurred on Jan. 15 2018 (20180115) at SQL city so my query to find the report from the table crime_scene_report will be :
@@ -90,21 +81,48 @@ ORDER BY rootpage;
 --78193	Maxine Whitely	183779	110	Fisk Rd	137882671
 
 --Jeremy Bowers is member of the Gym with memebership id starting from 48Z, and his plate number includes H42W.
---Jeremy Bowers is the murderes based on the above queries
+--Jeremy Bowers is the murderer based on the above queries
 --INSERT INTO solution VALUES (1, 'Jeremy Bowers');
         
         --SELECT value FROM solution;
         --The above guery gives me following result:
         -- Congrats, you found the murderer! But wait, there's more... If you think you're up for a challenge, try querying the interview transcript of the murderer to find the real villain behind this crime. If you feel especially confident in your SQL skills, try to complete this final step with no more than 2 queries. Use this same INSERT statement with your new suspect to check your answer.
  
- --Next step is to find the transcript and look for the real villain.
+ --Next step is to find the transcript and look for the real villain behind this murder.
 
 
-    
+-- Transcript query : SELECT * FROM interview WHERE person_id = 67318;
+--for Jeremy peters
+--SELECT * FROM interview WHERE person_id = 67318;
+--o/p: 67318	I was hired by a woman with a lot of money. I don't know her name but I know she's around 5'5" (65") or 5'7" (67"). She has red hair and she drives a Tesla Model S. I know that she attended the SQL Symphony Concert 3 times in December 2017.
 
---   1. The SQL queries you ran
---   2. Any notes or insights as SQL comments
---   3. Your final conclusion: who did it?
+--Using the above information we look up the description in the drivers_license table
+-- SELECT *
+  --FROM drivers_license
+ --where hair_color ='red' AND gender = 'female' AND car_make ='Tesla';
+ --o/p:
+--id	age	height	eye_color	hair_color	gender	plate_number	car_make	car_model
+--202298	68	66	green	red	female	500123	Tesla	Model S
+--291182	65	66	blue	red	female	08CM64	Tesla	Model S
+--918773	48	65	black	red	female	917UU3	Tesla	Model S
 
--- Using these SQL clauses will help you solve the mystery: 
---    SELECT, FROM, WHERE, AND, OR, ORDER BY, LIMIT, LIKE, DISTINCT, BETWEEN, AS
+-- The first person with  license id 202298 has height(66) between 65 and 67
+-- When we look up the license_id in person table
+--SELECT * FROM person WHERE license_id=202298 ;
+--o/p :99716	Miranda Priestly	202298	1883	Golden Ave	987756388
+--We get the above output.
+--To verify if she went to the symphony concert we use the following query
+--SELECT * FROM facebook_event_checkin WHERE person_id=99716;
+--We get the following o/p:
+--person_id	event_id	event_name	date
+--99716	1143	SQL Symphony Concert	20171206
+--99716	1143	SQL Symphony Concert	20171212
+--99716	1143	SQL Symphony Concert	20171229
+--This confirms Miranda Priestly has been to the SQL Symphony concert thrice in December 2017.
+--Try checking her name in the solution query
+--INSERT INTO solution VALUES (1, 'Miranda Priestly');
+--SELECT value FROM solution;
+--We get the following output
+--Congrats, you found the brains behind the murder! Everyone in SQL City hails you as the greatest SQL detective of all time. Time to break out the champagne!
+--Mystery is solved and Villian is found!!!
+
