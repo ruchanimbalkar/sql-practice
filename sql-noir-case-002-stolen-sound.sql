644781c00c5273c685d563ef8dd5bc359b44c87a
Rucha Nimbalkar <rucha.nimbalkar90@gmail.com>
2025-10-15 12:22:29 -0700

    added case background
    Added background information for case #002 involving a stolen vinyl record.

--- sql-noir-case-002-stolen-sound.sql
+++ sql-noir-case-002-stolen-sound.sql
@@ -2,3 +2,11 @@
   -- 1. The SQL queries you ran
   -- 2. Any notes or insights as SQL comments
   -- 3. Your final conclusion: who did it?
+-- Background :
+-- Case #002: The Stolen Sound
+-- In the neon glow of 1980s Los Angeles, the West Hollywood Records store was rocked by a daring theft. 
+  --A prized vinyl record, worth over $10,000, vanished during a busy evening, leaving the store owner desperate for answers. 
+  --Vaguely recalling the details, you know the incident occurred on July 15, 1983, at this famous store. 
+  --Your task is to track down the thief and bring them to justice.
+
+
