cbb3eb49c3adeb8f69863e178d204a9f84ff92cf
Rucha Nimbalkar <rucha.nimbalkar90@gmail.com>
2025-10-16 12:20:19 -0700

    Enhance SQL file with case background and schema
    Added background information and schema details for the Vanishing Diamond case.

--- sql-noir-case-004-vanishing-diamond.sql
+++ sql-noir-case-004-vanishing-diamond.sql
@@ -2,3 +2,13 @@
   -- 1. The SQL queries you ran
   -- 2. Any notes or insights as SQL comments
   -- 3. Your final conclusion: who did it?
+-- Background :
+    -- Case #006: The Vanishing Diamond
+    -- At Miami’s prestigious Fontainebleau Hotel charity gala, the famous “Heart of Atlantis” diamond necklace suddenly disappeared from its display.
+--Tables/schema :
+  --crime_scene
+  --guest
+  --witness_statements
+  --attire_registry
+  --marina_rentals
+  --final_interviews
