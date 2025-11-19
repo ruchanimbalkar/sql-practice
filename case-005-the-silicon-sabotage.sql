e4f52860264de86e7d30a1b23117aa54b2e03657
Rucha Nimbalkar <rucha.nimbalkar90@gmail.com>
2025-11-19 12:57:14 -0800

    Add query to analyze employee access logs
    Added a new query to analyze keycard access logs and computer access logs for employees with specific criteria.

--- case-005-the-silicon-sabotage.sql
+++ case-005-the-silicon-sabotage.sql
@@ -20,4 +20,26 @@ WHERE incident_id = 74;
 -- id	incident_id	employee_id	  statement
 -- 40	74	        145	          I heard someone mention a server in Helsinki.
 -- 59	74	        134	          I saw someone holding a keycard marked QX- succeeded by a two-digit odd number.
+-- Notes/Comments : I then started to look into Helsinki and the key_card_access_logs
+-- Then I wrote the following query :
+-- Query #3
+WITH results AS (SELECT *
+FROM keycard_access_logs 
+RIGHT JOIN employee_records
+ON keycard_access_logs.employee_id = employee_records.id
+WHERE keycard_code LIKE 'QX%'
+AND access_date= 19890421)
+SELECT distinct results.employee_name, results.employee_id
+FROM results
+JOIN computer_access_logs
+ON results.employee_id = computer_access_logs.employee_id
+WHERE
+server_location LIKE '%Helsinki%';
+-- The above query gave me the following results :
+-- employee_name	employee_id
+-- Margie Weber	178
+-- Ann Peterson	56
+-- Paul Adams	23
+-- Elizabeth Gordon	99
+-- Notes/ Comments : Next I am going to look at the email conversation of the above employees
 
