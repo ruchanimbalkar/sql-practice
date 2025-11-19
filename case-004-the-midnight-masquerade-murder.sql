70d0e784c463ce7ef0e0c1041a587eb922ef2f4e
Rucha Nimbalkar <rucha.nimbalkar90@gmail.com>
2025-11-19 09:30:42 -0800

    Add query to fetch crime scene information
    Added initial query to retrieve crime scene details.

--- case-004-the-midnight-masquerade-murder.sql
+++ case-004-the-midnight-masquerade-murder.sql
@@ -1,3 +1,10 @@
 -- Backstory : On October 31, 1987, at a Coconut Grove mansion masked ball, 
               -- Leonard Pierce was found dead in the garden.
               -- Can you piece together all the clues to expose the true murderer?
+-- Query #1
+SELECT * FROM crime_scene where date=19871031 AND location='Miami Mansion, Coconut Grove';
+-- The above query gives the following result :
+-- id	date	location	description
+-- 75	19871031	Miami Mansion, Coconut Grove	During a masked ball, 
+--   a body was found in the garden. Witnesses mentioned a hotel booking 
+--   and suspicious phone activity.
