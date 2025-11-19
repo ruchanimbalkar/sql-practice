b710916c3a4fd44095e5485b9149b0ecba760aac
Rucha Nimbalkar <rucha.nimbalkar90@gmail.com>
2025-11-19 12:18:19 -0800

    Add backstory for case 005: Silicon Sabotage
    Added backstory for the case involving QuantumTech.

---
+++ case-005-the-silicon-sabotage.sql
@@ -0,0 +1,4 @@
+-- Backstory : 
+-- QuantumTech, Miami’s leading technology corporation, was about to unveil its groundbreaking microprocessor called “QuantaX.”
+--   Just hours before the reveal, the prototype was destroyed, and all research data was erased. Detectives suspect corporate espionage.
+
